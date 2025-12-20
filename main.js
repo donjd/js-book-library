b033012181f131c75bb7278e8a1485dae576737b
donjd <99505859+donjd@users.noreply.github.com>
2025-12-19 23:02:46 -0700

    Rewrite function that adds book to the library
    I made the previous function after adding all of the books to the
    libary array and so it was a weird woorkaround to get only the newest book
    added to the libary. I had a full array and then was iterating through
    it to add the book, but only if it was a new book.
    
    This new function pushes the newBook to the library and then appends
    it to the library. Much simpler.

--- main.js
+++ main.js
@@ -1,69 +1,33 @@
-window.addEventListener("load", () => {
-  displayBooksInLibrary();
-});
+// window.addEventListener("load", () => {
+//   displayBooksInLibrary();
+// });
 
 const library = [];
 
 function Book(title, author) {
   this.title = title;
   this.author = author;
-  this.id = createUniqueId();
+  this.id = crypto.randomUUID();
 }
 
 Book.prototype.displayBookInfo = function () {
   return `Title: ${this.title}, Author: ${this.author}, Book ID: ${this.id}`;
 };
 
-function createUniqueId() {
-  return crypto.randomUUID();
-}
-
 function addBookToLibrary(...books) {
   library.push(...books);
 }
 
 const libraryGrid = document.querySelector("#library");
-
-function displayBooksInLibrary() {
-  if (!libraryGrid) return;
-  libraryGrid.innerHTML = "";
-
-  library.forEach((book) => {
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("bookCard");
-
-    const bookContent = document.createElement("div");
-    bookContent.classList.add("bookContent");
-
-    bookContent.textContent = book.title;
-
-    bookCard.appendChild(bookContent);
-    libraryGrid.appendChild(bookCard);
-  });
-}
-
-const hungerGames = new Book("Hunger Games", "Suzanne Collins");
-const hatchet = new Book("Hatchet", "Gary Paulsen");
-const gooseGirl = new Book("Goose Girl", "Shannon Hale");
-const afterDark = new Book("After Dark", "Murakami");
-const mazeRunner = new Book("Maze Runner", "Author");
-
-addBookToLibrary(hungerGames, hatchet, gooseGirl, afterDark, mazeRunner);
-
-// on page load, determine lenght of libary array and create that many
-// cards.
-//
-
 const addBookBtn = document.querySelector(".add-book-btn");
 const form = document.querySelector("form");
+const addBookToLibraryBtn = document.querySelector(".add-book-to-library-btn");
+const bookTitle = document.querySelector(".book-title");
+const bookAuthor = document.querySelector(".book-author");
 
 addBookBtn.addEventListener("click", () => {
   form.classList.toggle("open");
 });
-
-const addBookToLibraryBtn = document.querySelector(".add-book-to-library-btn");
-const bookTitle = document.querySelector(".book-title");
-const bookAuthor = document.querySelector(".book-author");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -73,7 +37,21 @@ form.addEventListener("submit", (e) => {
 
   const newBook = new Book(newBookTitle, newBookAuthor);
 
-  addBookToLibrary(newBook);
-  displayBooksInLibrary();
+  library.push(newBook);
+
+  const bookCard = document.createElement("div");
+  const bookContent = document.createElement("div");
+
+  bookCard.classList.add("bookCard");
+  bookContent.classList.add("bookContent");
+
+  bookContent.textContent = newBook.title;
+
+  bookCard.appendChild(bookContent);
+  libraryGrid.appendChild(bookCard);
+
+  // addBookToLibrary(newBook);
+  // displayBooksInLibrary();
   form.reset();
+  console.log(library);
 });
