cf7fe993ca7e069fea603e984b8a04bd3e0e25a2
donjd <99505859+donjd@users.noreply.github.com>
2025-12-23 00:00:58 -0700

    Clean up code to remove from array and dom

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
 const library = [];
 
-function Book(title, author) {
+//Establish book constructor and methods
+function Book(title, author, haveRead, isbn) {
   this.title = title;
   this.author = author;
+  this.haveRead = haveRead;
+  this.isbn = isbn;
   this.id = crypto.randomUUID();
 }
 
-Book.prototype.displayBookInfo = function () {
-  return `Title: ${this.title}, Author: ${this.author}, Book ID: ${this.id}`;
-};
+function addToLibrary(book) {
+  library.push(book);
+}
 
+function removeBookById(id) {
+  const indexOfBook = library.findIndex((book) => book.id === id);
+  if (indexOfBook !== -1) {
+    library.splice(indexOfBook, 1);
+  }
+}
+
+//DOM selectors
 const libraryGrid = document.querySelector("#library");
 const addBookBtn = document.querySelector(".add-book-btn");
 const form = document.querySelector("form");
-const addBookToLibraryBtn = document.querySelector(".add-book-to-library-btn");
 const bookTitle = document.querySelector(".book-title");
 const bookAuthor = document.querySelector(".book-author");
 
+//shows and hides the add book section
 addBookBtn.addEventListener("click", () => {
   form.classList.toggle("open");
 });
@@ -24,17 +35,14 @@ addBookBtn.addEventListener("click", () => {
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  //exits the form unless these are both filled out
   if (!bookTitle.value || !bookAuthor.value) {
     return;
   }
 
-  const newBookTitle = bookTitle.value;
-  const newBookAuthor = bookAuthor.value;
+  const newBook = new Book(bookTitle.value, bookAuthor.value);
 
-  const newBook = new Book(newBookTitle, newBookAuthor);
-
-  library.push(newBook);
-
+  //creates book elements
   const bookCard = document.createElement("div");
   const bookContent = document.createElement("div");
   const removeBookXBtn = document.createElement("button");
@@ -43,15 +51,21 @@ form.addEventListener("submit", (e) => {
   bookContent.classList.add("bookContent");
   removeBookXBtn.classList.add("remove-book-x-btn");
 
+  //adds book id to both the book and the X button to be
+  //associated together later if needed
   bookCard.dataset.bookId = newBook.id;
   removeBookXBtn.dataset.bookId = newBook.id;
 
   bookContent.textContent = newBook.title;
   removeBookXBtn.textContent = "X";
 
+  //adds book to the DOM library
   bookCard.appendChild(bookContent);
   bookCard.appendChild(removeBookXBtn);
   libraryGrid.appendChild(bookCard);
+
+  //adds book to the library array
+  addToLibrary(newBook);
 
   bookCard.addEventListener("mouseover", () => {
     removeBookXBtn.classList.add("x-btn-visible");
@@ -65,9 +79,11 @@ form.addEventListener("submit", (e) => {
   bookTitle.focus();
 });
 
+//removes book from library array and dom library
 libraryGrid.addEventListener("click", (e) => {
   if (e.target.classList.contains("remove-book-x-btn")) {
-    const matchingBook = e.target.closest(".bookCard");
-    matchingBook.remove();
+    const bookId = e.target.dataset.bookId;
+    removeBookById(bookId);
+    e.target.closest(".bookCard").remove();
   }
 });
